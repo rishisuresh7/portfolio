69e72de88213b7c966d4822e3909a297fc6c8908
Rishi Suresh <rishi9632@gmail.com>
2020-05-29 22:56:29 +0530

    Basic project setup using webpack and babel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import App from './App.js';
+import './index.css';
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules : [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            },
+            {
+                test: /\.(c|sc)ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+              },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: "./src/index.html",
+          filename: "./index.html"
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+        transportMode: 'ws'
+    }
+}
