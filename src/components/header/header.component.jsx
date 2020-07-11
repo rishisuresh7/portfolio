e7d4259bc0c6546246b76848be4ec3ef61d49fee
Rishi Suresh <rishi9632@gmail.com>
2020-07-11 22:32:12 +0530

    add animation to logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './header.styles.scss';
 
 
 const Header = () => {
+
+    useEffect(() => {
+        const elements = document.querySelectorAll('#animate-header');
+        let t = 20;
+        const addAnimation = (i = 0) => {
+            if (i >= 4) {
+                return
+            } else {
+                t += 20;
+                elements[i].classList.add('fadein');
+            }
+
+            setTimeout(() => {
+                addAnimation(++i);
+            }, t)
+        }
+
+        addAnimation();
+    }, []);
 
     const handleClick = (event) => {
         event.preventDefault();
@@ -18,13 +37,13 @@ const Header = () => {
     return (
         <div className="header">
            <div className="header-content">
-            <div className="header-logo">
+            <div className="header-logo" id="animate-header">
                     <span className="header-text">RS</span>
             </div>
             <div className="header-options">
-                    <a className = "header-option" href="profile" onClick={handleClick}>&nbsp;&nbsp;Home</a>
-                    <a className = "header-option" href="experience" onClick={handleClick}>&nbsp;&nbsp;Experience</a>
-                    <a className = "header-option" href="projects" onClick={handleClick}>&nbsp;&nbsp;Projects</a>
+                    <a className = "header-option" href="profile" onClick={handleClick} id="animate-header"><span>&nbsp;&nbsp;Home</span></a>
+                    <a className = "header-option" href="experience" onClick={handleClick} id="animate-header"><span>&nbsp;&nbsp;Experience</span></a>
+                    <a className = "header-option" href="projects" onClick={handleClick} id="animate-header"><span>&nbsp;&nbsp;Projects</span></a>
             </div>
            </div>
         </div>
