3be6b649daee1166886f2b5874c3a3f392fa9378
Rishi Suresh <rishi9632@gmail.com>
2020-07-16 13:46:50 +0530

    add techStack reducer - move languages data to redux store

--- src/components/tech-stack/tech-stack.component.jsx
+++ src/components/tech-stack/tech-stack.component.jsx
@@ -1,80 +1,9 @@
 import React, {useEffect} from 'react';
-import './tech-stack.styles.scss';
+import {connect} from 'react-redux';
 import Chip from '../chip/chip.component.jsx';
+import './tech-stack.styles.scss';
 
-const TechStack = () => {
-    const techStack = [
-        {
-            id: 1,
-            name: 'Golang',
-            icon: './assets/img/go-avatar.png'
-        },
-        {
-            id: 2,
-            name: 'Neo4j',
-            icon: './assets/img/neo4j-avatar.png'
-        },
-        {
-            id: 3,
-            name: 'JavaScript',
-            icon: './assets/img/js-avatar.png'
-        },
-        {
-            id: 4,
-            name: 'React.js',
-            icon: './assets/img/react-avatar.png'
-        },
-        {
-            id: 5,
-            name: 'Redux',
-            icon: './assets/img/redux-avatar.png'
-        },
-        {
-            id: 6,
-            name: 'Git',
-            icon: './assets/img/git-avatar.png'
-        },
-        {
-            id: 7,
-            name: 'Docker',
-            icon: './assets/img/docker-avatar.png'
-        },
-        {
-            id: 8,
-            name: 'Java',
-            icon: './assets/img/java-avatar.png'
-        },
-        {
-            id: 9,
-            name: 'Python',
-            icon: './assets/img/python-avatar.png'
-        },
-        {
-            id: 10,
-            name: 'Linux',
-            icon: './assets/img/linux-avatar.png'
-        },
-        {
-            id: 11,
-            name: 'HTML',
-            icon: './assets/img/html-avatar.png'
-        },
-        {
-            id: 12,
-            name: 'CSS/SCSS',
-            icon: './assets/img/css-avatar.png'
-        },
-        {
-            id: 13,
-            name: 'Docker Compose',
-            icon: './assets/img/docker-compose-avatar.png'
-        },
-        {
-            id: 14,
-            name: 'Jenkins',
-            icon: './assets/img/jenkins-avatar.png'
-        },
-    ];
+const TechStack = ({techStack}) => {
 
     const  getRandomIndex = (min, max) => {
         return Math.floor(Math.random() * (max - min)) + min;
@@ -119,4 +48,8 @@ const TechStack = () => {
     </div>
 }
 
-export default TechStack;
+const mapStateToProps = state => ({
+    techStack: state.languages.techStack,
+})
+
+export default connect(mapStateToProps)(TechStack);
--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -3,12 +3,14 @@ import setBackgroundReducer from '../components/settings/settings.reducer.jsx';
 import userDetails from '../components/profile/profile.reducer.jsx';
 import setExperience from '../components/experience/experience.reducer.jsx';
 import projectReducer from '../components/projects/projects.reducer.jsx';
+import techStackReducer from '../components/tech-stack/tech-stack.reducer.jsx';
 
 const rootReducer =  combineReducers({
     globalStyle: setBackgroundReducer,
     user: userDetails,
     experience: setExperience,
     project: projectReducer,
+    languages: techStackReducer,
 });
 
 export default rootReducer;
